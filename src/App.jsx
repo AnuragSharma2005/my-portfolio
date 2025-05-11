0e4bd391c5b1eb24df7c6b96b30b99bd48d64051
Anurag Sharma <anuragwork2005@gmail.com>
2025-05-11 19:02:46 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Achievements from './components/achievements';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 function App() {
   return (
