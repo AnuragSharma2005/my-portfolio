358271fb3ee21746b79c7eb1e6bee3b53bce0e60
Anurag Sharma <anuragwork2005@gmail.com>
2025-05-11 18:46:03 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
             </p>
             <div className="flex gap-4">
               <a
-                href="/my-portfolio/Anu%20new%20resume.pdf"
+                href="/my-portfolio/my%20portfolio%20resume.docx"
                 className="px-6 py-3 bg-[#4B2E2E] text-white rounded-xl font-semibold hover:bg-[#4B2E2E] transition"
                 download
               >
