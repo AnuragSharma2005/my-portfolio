95b122ba18a0a33e08d48e6f6cce57bc8b9f9e58
Anurag Sharma <anuragwork2005@gmail.com>
2025-05-11 19:05:37 +0530

    added build pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/my-portfolio/',
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-slick']
+    include: ['react-slick', 'lucide-react']
   }
 })
